a9122b07d1671fd1bb8f91195ea8799dbff4acbc
Paulina Grunwald <paulina.grunwald@quasset.com>
2018-11-09 14:27:57 +0100

    Install Redux and add shallow test for App component.

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,15 +1,9 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { shallow } from 'enzyme'
 import App from '../App'
+import CommentBox from '../CommentBox'
 
 it('shows a comment box', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
-  // Looks inside the div
-  // and checks to see if the CommentBox exits in the App
-  // console.log(div.innerHTML)
-  expect(div.innerHTML).toContain('Comment Box')
-  // clean up after test
-  ReactDOM.unmountComponentAtNode(div)
+  const wrapped = shallow(<App />)
+  expect(wrapped.find(CommentBox).length).toEqual(1)
 })
- 
