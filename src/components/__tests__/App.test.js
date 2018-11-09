8f44f61eb708100c71f4075162c823b88ff11a4b
Paulina Grunwald <paulina.grunwald@quasset.com>
2018-11-09 14:40:20 +0100

    Add .env and set up  Absolute Path Imports. Update all import paths.

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import App from '../App'
-import CommentBox from '../CommentBox'
+import App from 'components/App'
+import CommentBox from 'components/CommentBox'
+import CommentList from 'components/CommentList'
 
 it('shows a comment box', () => {
   const wrapped = shallow(<App />)
   expect(wrapped.find(CommentBox).length).toEqual(1)
+})
+
+it('shows a comment list', () => {
+  const wrapped = shallow(<App />)
+  expect(wrapped.find(CommentList).length).toEqual(1)
 })
