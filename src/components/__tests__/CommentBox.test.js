90672da01fd6d40cc00434db3dc6237e31e58d46
Paulina Grunwald <paulina.grunwald@quasset.com>
2018-11-10 08:56:20 +0100

    Add test to check if user can enter input intu text area and check if the text area is emptied when form submitted.

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -17,7 +17,29 @@ afterEach(() => {
   wrapped.unmount()
 })
 
-it('shas text area and a button', () => {
+it('has text area and a button', () => {
   expect(wrapped.find('textarea').length).toEqual(1)
   expect(wrapped.find('button').length).toEqual(1)
+})
+
+// with describe you can
+describe('the text area', () => {
+  beforeEach(() => {
+    wrapped.find('textarea').simulate('change', {
+      target: { value: 'new comment' }
+    })
+    wrapped.update()
+  })
+
+  it('has a text area in which user can type in', () => {
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment')
+  })
+
+  it('when form is submitted, text area gets emptied', () => {
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment')
+    wrapped.find('form').simulate('submit')
+    wrapped.update()
+
+    expect(wrapped.find('textarea').prop('value')).toEqual('')
+  })
 })
