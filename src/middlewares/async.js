f5cae2b363885755d0c4c9d49e9f6acd40eb3cf7
Paulina Grunwald <paulina.grunwald@quasset.com>
2018-11-17 18:38:01 +0100

    create middleware that checks if the state has the right format. Create json schema and install tv4 validator.

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -6,7 +6,6 @@ export default ({ dispatch }) => next => action => {
   if (!action.payload || !action.payload.then) {
     return next(action)
   }
-
   // We want to wait for the promise to resolve
   // (get the data) and then create a new
   // action with that data and dispatch it
