b238520035ac5a95d4f690defba52182c5cc1772
Paulina Grunwald <paulina.grunwald@quasset.com>
2018-11-23 21:01:42 +0100

    archive old client side files

---
+++ archived/components/__tests__/CommentBox.test.js
@@ -0,0 +1,49 @@
+/**
+ * @jest-environment jsdom
+*/
+
+import React from 'react'
+// mount for Full DOM Rendering
+import { mount } from 'enzyme'
+import CommentBox from 'components/CommentBox'
+import Root from 'Root'
+let wrapped
+
+beforeEach(() => {
+  wrapped = mount(
+    <Root>
+      <CommentBox />
+    </Root>
+  )
+})
+
+afterEach(() => {
+  wrapped.unmount()
+})
+
+it('has text area and a button', () => {
+  expect(wrapped.find('textarea').length).toEqual(1)
+  expect(wrapped.find('button').length).toEqual(2)
+})
+
+// with describe you can run multiple tests at the same time
+describe('the text area', () => {
+  beforeEach(() => {
+    wrapped.find('textarea').simulate('change', {
+      target: { value: 'new comment' }
+    })
+    wrapped.update()
+  })
+
+  it('has a text area in which user can type in', () => {
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment')
+  })
+
+  it('when form is submitted, text area gets emptied', () => {
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment')
+    wrapped.find('form').simulate('submit')
+    wrapped.update()
+
+    expect(wrapped.find('textarea').prop('value')).toEqual('')
+  })
+})
