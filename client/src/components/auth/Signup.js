b3573fcfb974f3dd8e16d1fa36f7bd35d54378f2
Paulina Grunwald <paulina.grunwald@quasset.com>
2018-11-26 20:19:03 +0100

    add routes and new Signup component

---
+++ client/src/components/auth/Signup.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+class Signup extends Component {
+  render () {
+    return (
+      <form>
+        <fieldset>
+          <label>E-mail</label>
+        </fieldset>
+        <fieldset>
+          <label>Passport</label>
+        </fieldset>
+      </form>
+    )
+  }
+}
+
+export default Signup
--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from 'components/App'
+
 import { BrowserRouter, Route } from 'react-router-dom'
+import Signup from './components/auth/Signup'
+import Welcome from './components/Welcome'
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <App>
+      <Route path='/' exact component={Welcome} />
+      <Route path='/signup' exact component={Signup} />
+    </App>
   </BrowserRouter>,
-  document.getElementById('root')
+  document.querySelector('#root')
 )
