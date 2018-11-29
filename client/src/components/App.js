5d9d83a287ea5e95152971f048f683375db2444a
Paulina Grunwald <paulina.grunwald@quasset.com>
2018-11-29 06:41:29 +0100

    modify App component and add children as a prop.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import Header from './Header'
 
-export default () => {
+export default ({ children }) => {
   return (
     <div>
       <Header />
-      Hello
+      {children}
     </div>
   )
 }
