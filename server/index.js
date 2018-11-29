36a84b05c394d835d72244f5fea9146179f466af
Paulina Grunwald <paulina.grunwald@quasset.com>
2018-11-29 15:50:55 +0100

    Install CORS and wire it up to Express

--- server/index.js
+++ server/index.js
@@ -5,12 +5,14 @@ const morgan = require('morgan')
 const app = express()
 const router = require('./router')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 // DB Setup
 mongoose.connect('mongodb://localhost/auth')
 // App setup
 app.use(morgan('combined'))
 app.use(bodyParser.json({ type: '*/*' }))
+app.use(cors())
 router(app)
 // Server Setup
 const port = process.env.PORT || 3090
